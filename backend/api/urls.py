358c44b9d08f72b6322c99097cce1b2edbb757a7
alex <aleksandrsavin03@mail.ru>
2023-03-17 00:05:34 +0500

    Переработана структура

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,7 +3,6 @@ from rest_framework.routers import DefaultRouter
 from djoser.views import TokenCreateView, TokenDestroyView
 
 from foodgram.views import IngredientViewSet, TagViewSet, RecipeViewSet
-# from .views import IngredientViewSet, TagViewSet, RecipeViewSet
 from users.views import UserViewSet
 
 router = DefaultRouter()
@@ -13,10 +12,7 @@ router.register('recipes', RecipeViewSet, basename='recipes')
 router.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
-    # path('auth/', include('djoser.urls.authtoken')),
     path('', include(router.urls)),
-    # path('', include('djoser.urls')),
-
     path('auth/token/login/', TokenCreateView.as_view()),
     path('auth/token/logout/', TokenDestroyView.as_view()),
 ]
